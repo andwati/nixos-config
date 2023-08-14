1fcc378c99efea445636e9aa8676a8d49707e202
Ian Andwati <andwatiian@gmail.com>
2023-08-14 14:48:54 +0300

    added git configurations

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<space>w', '<cmd>write<cr>', {desc = 'save'})
